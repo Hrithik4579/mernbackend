01fc578fcb9cf11eb8205e3a3ecac1519bc25e19
Hrithik4579 <116161461+Hrithik4579@users.noreply.github.com>
2024-11-22 13:54:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-};
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //routes import
 import student from './routes/student.route.js'
